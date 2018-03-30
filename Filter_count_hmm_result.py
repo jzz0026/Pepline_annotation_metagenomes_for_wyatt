291af88e6652391fcc48a9579521cc2cf8ea48e6
Jinglie Zhou <jzz0026@lbl.gov>
2018-03-30 13:18:47 -0700

    modified: sorted by score

--- Filter_count_hmm_result.py
+++ Filter_count_hmm_result.py
@@ -1,36 +1,21 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
 import pandas as pd
+## sort hmm result and get the top hit for each read (sorted by Score)
+
 from sys import argv
 
 script, file_name = argv
-#file_name = "Browns_ThreeSqA_D1_extractCH4.tab"
 
-
-# In[3]:
 
 
 df = pd.read_csv(file_name,sep="\t",header=None)
 df = df[0].str.split(" +",expand=True)
-df1 = df.iloc[:-10,[0,2,3,4]]
-df1.columns = ["Query_orfs","KO ID","HMM ID","E-value"]
+df1 = df.iloc[:-10,[0,2,3,4,5]]
+df1.columns = ["Query_orfs","KO ID","HMM ID","E-value","Score"]
 df1["Query"] = df1["Query_orfs"].str.split("_",expand=True)[0] 
-df2 = df1.sort_values(["Query","E-value"])
+df2 = df1.sort_values(["Query","Score"],ascending=False)
 df3 = df2.groupby("Query",as_index=False).first() ## get the best hit for each read!! not orf!!
 df3.to_csv(file_name.split(".")[0] +"_filtered_hmm.result")
 
-
-# In[6]:
-
-
-#df3 = pd.read_csv(file_name.split(".")[0] +"_filtered_hmm.result",index_col=0)
-
-
-# In[31]:
 
 
 df3["KO ID"] = df3["KO ID"].str.replace("KO:","").str.replace("_.*","")
@@ -44,10 +29,3 @@ df5.columns = ["KO ID","HMM ID"]
 df6 = pd.DataFrame(df5.groupby("KO ID").size())
 df6.columns = ["Counts"]
 df6.to_csv(file_name.split(".")[0] +"_filtered_hmm.summary")
-
-
-# In[1]:
-
-
-#df_sum["KO ID"].str.replace("KO:","").str.replace("_.*,",",").str.replace("_.*","")
-
